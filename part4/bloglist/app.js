bdfe3a2b7a8819540d1a8e8d2072487384293988
seenicroot <sngmanhim@hotmail.com>
2021-04-20 22:52:35 -0400

    Refactor app, exercise 4.2

---
+++ part4/bloglist/app.js
@@ -0,0 +1,17 @@
+const config = require('./utils/config')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const blogsRouter = require('./controllers/blogs')
+const mongoose = require('mongoose')
+const logger = require('./utils/logger')
+
+const mongoUrl = config.MONGODB_URI
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+  .then(() => logger.info("connected to database"))
+
+app.use(cors())
+app.use(express.json())
+app.use('/api/blogs', blogsRouter)
+
+module.exports = app

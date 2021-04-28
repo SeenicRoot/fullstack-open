54500430e8b314eb4ad2bed93e5c27c576934b6b
seenicroot <sngmanhim@hotmail.com>
2021-04-27 21:36:17 -0400

    Finish exercises of part4b, exercise 4.14

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -13,70 +13,130 @@ beforeEach(async () => {
   await Promise.all(promiseArray)
 })
 
-test('all blogs are returned', async () => {
-  const response = await api.get('/api/blogs')
+describe('when there are blogs in the database', () => {
+  test('all blogs are returned', async () => {
+    const response = await api.get('/api/blogs')
 
-  expect(response.body).toHaveLength(helper.initialBlogs.length)
+    expect(response.body).toHaveLength(helper.initialBlogs.length)
+  })
+
+  test('blogs have the property "id"', async () => {
+    const allBlogs = await helper.blogsInDb()
+    expect(allBlogs[0].id).toBeDefined()
+  })
 })
 
-test('blogs have the property "id"', async () => {
-  const allBlogs = await helper.blogsInDb()
-  expect(allBlogs[0].id).toBeDefined()
+describe('addition of a blog', () => {
+  test('a blog post can be added', async () => {
+    const newBlog = {
+      title: 'Starting this newsletter, print debugging, BYOC',
+      author: 'Geoffrey Litt',
+      url: 'https://buttondown.email/geoffreylitt/archive/starting-this-newsletter-print-debugging-byoc/',
+      likes: 0,
+    }
+
+    const response = await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    const blogsAtEnd = await helper.blogsInDb()
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+    expect(blogsAtEnd).toContainEqual(response.body)
+  })
+
+  test('if blog likes property is missing, likes is set to 0', async() => {
+    const newBlog = {
+      title: 'Starting this newsletter, print debugging, BYOC',
+      author: 'Geoffrey Litt',
+      url: 'https://buttondown.email/geoffreylitt/archive/starting-this-newsletter-print-debugging-byoc/',
+    }
+
+    const response = await api
+      .post('/api/blogs')
+      .send(newBlog)
+    
+    expect(response.body).toHaveProperty('likes', 0)
+  })
+
+  test('blog is required to have a url and title', async () => {
+    const noUrlBlog = {
+      title: 'Starting this newletter, print debugging, BYOC',
+      author: 'Geoffrey Litt',
+      likes: 0,
+    }
+    const noTitleBlog = {
+      author: 'Geoffrey Litt',
+      url: 'https://buttondown.email/geoffreylitt/archive/starting-this-newsletter-print-debugging-byoc/',
+      likes: 0,
+    }
+
+    await api
+      .post('/api/blogs')
+      .send('noUrlBlog')
+      .expect(400)
+    await api
+      .post('/api/blogs')
+      .send('noTitleBlog')
+      .expect(400)
+  })
 })
 
-test('a blog post can be added', async () => {
-  const newBlog = {
-    title: 'Starting this newsletter, print debugging, BYOC',
-    author: 'Geoffrey Litt',
-    url: 'https://buttondown.email/geoffreylitt/archive/starting-this-newsletter-print-debugging-byoc/',
-    likes: 0,
-  }
+describe('deletion of a blog', () => {
+  test('succeeds with status code 204 if id is valid', async () => {
+    const blogsAtStart = await helper.blogsInDb()
+    const blogToDelete = blogsAtStart[0]
 
-  const response = await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/)
+    await api
+      .delete(`/api/blogs/${blogToDelete.id}`)
+      .expect(204)
 
-  const blogsAtEnd = await helper.blogsInDb()
-  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
-  expect(blogsAtEnd).toContainEqual(response.body)
+    const blogsAtEnd = await helper.blogsInDb()
+
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length - 1)
+    expect(blogsAtEnd).not.toContainEqual(blogToDelete)
+  })
 })
 
-test('if blog likes property is missing, set likes to 0', async() => {
-  const newBlog = {
-    title: 'Starting this newsletter, print debugging, BYOC',
-    author: 'Geoffrey Litt',
-    url: 'https://buttondown.email/geoffreylitt/archive/starting-this-newsletter-print-debugging-byoc/',
-  }
+describe('update of a blog', () => {
+  test('succeeds with status 200 if id is valid', async() => {
+    const blogs = await helper.blogsInDb()
+    const originalBlog= blogs[0]
+    const newBlog = {
+      likes: 100000,
+    }
+    await api
+      .put(`/api/blogs/${originalBlog.id}`)
+      .send(newBlog)
+      .expect(200)
 
-   const response = await api
-    .post('/api/blogs')
-    .send(newBlog)
-  
-  expect(response.body).toHaveProperty('likes', 0)
-})
+    const badId = 12345
+    await api
+      .put(`/api/blogs/${badId}`)
+      .send(newBlog)
+      .expect(400)
+  })
 
-test('blog has to have a url and title', async () => {
-  const noUrlBlog = {
-    title: 'Starting this newletter, print debugging, BYOC',
-    author: 'Geoffrey Litt',
-    likes: 0,
-  }
-  const noTitleBlog = {
-    author: 'Geoffrey Litt',
-    url: 'https://buttondown.email/geoffreylitt/archive/starting-this-newsletter-print-debugging-byoc/',
-    likes: 0,
-  }
+  test('only updates the likes property of a blog', async () => {
+    const blogs = await helper.blogsInDb()
+    const originalBlog = blogs[0]
+    const newBlog = {
+      title: 'new title',
+      author: 'new author',
+      url: 'new url',
+      likes: 1000000,
+    }
+    
+    const updatedBlog = await api
+      .put(`/api/blogs/${originalBlog.id}`)
+      .send(newBlog)
 
-  await api
-    .post('/api/blogs')
-    .send('noUrlBlog')
-    .expect(400)
-  await api
-    .post('/api/blogs')
-    .send('noTitleBlog')
-    .expect(400)
+    expect(updatedBlog.body).toHaveProperty('likes', newBlog.likes)
+    expect(updatedBlog.body).toHaveProperty('author', originalBlog.author)
+    expect(updatedBlog.body).toHaveProperty('title', originalBlog.title)
+    expect(updatedBlog.body).toHaveProperty('url', originalBlog.url)
+  })
 })
 
 afterAll(() => {
--- part4/bloglist/utils/middleware.js
+++ part4/bloglist/utils/middleware.js
@@ -2,10 +2,10 @@ const logger = require('./logger')
 
 const errorHandler = (error, request, response, next) => {
   if (error.name === 'CastError') {
-    return response.send(400)
+    return response.status(400).end()
   } 
   if (error.name === 'ValidationError') {
-    return response.send(400)
+    return response.status(400).end()
   }
 }
 

39edcb0a5df258569bc1135d146f3669827b833e
seenicroot <sngmanhim@hotmail.com>
2021-05-24 21:13:26 -0400

    Add hide/view button to blog posts and improve styling

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Blog = ({blog}) => {
+  const [showDetails, setShowDetails] = useState(false)
+
+  const handleShowDetails = () => {
+    setShowDetails(!showDetails)
+  }
+
+  const blogStyle = {
+    border: '1px solid black',
+    padding: '5px 6px',
+    margin: '3px 0',
+  }
+
+  if (showDetails) {
+    return (
+      <div style={blogStyle}>
+        <h4 style={{display: 'inline'}}>{blog.title}</h4> <button onClick={handleShowDetails}>hide</button><br />
+        <a href={blog.url}>{blog.url}</a><br />
+        {blog.likes} likes <button>like</button><br />
+        {blog.author}
+      </div>
+    )
+  }
   return (
-    <div>
-      <a href={blog.url}>{blog.title} - {blog.author}</a>
+    <div style={blogStyle}>
+      {blog.title} - {blog.author} <button onClick={handleShowDetails}>view</button>
     </div>
   )
 }

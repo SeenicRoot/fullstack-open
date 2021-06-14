7cb7f6f6cef647dd093d4653ab14669ab48c2336
seenicroot <sngmanhim@hotmail.com>
2021-06-14 01:07:14 -0400

    Remove redundant case from blogReducer

--- part7/bloglist-frontend/src/reducers/blogReducer.js
+++ part7/bloglist-frontend/src/reducers/blogReducer.js
@@ -2,8 +2,6 @@ import blogService from '../services/blogs'
 
 const reducer = (state=[], action) => {
   switch (action.type) {
-    case 'INIT_BLOGS':
-      return action.data
     case 'SET_BLOGS':
       return action.data
     case 'ADD_BLOG':
@@ -17,7 +15,7 @@ export const initBlogs = () => {
   return async (dispatch) => {
     const blogs = await blogService.getAll()
     dispatch({
-      type: 'INIT_BLOGS',
+      type: 'SET_BLOGS',
       data: blogs,
     })
   }

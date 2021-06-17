88f8e71b4b2e979a6f62eb25347c1127037aa6a2
seenicroot <sngmanhim@hotmail.com>
2021-06-17 01:57:59 -0400

    Style page of individual users

--- part7/bloglist-frontend/src/components/User.js
+++ part7/bloglist-frontend/src/components/User.js
@@ -1,4 +1,9 @@
 import React from 'react'
+import {
+  Header,
+  List,
+  Divider,
+} from 'semantic-ui-react'
 
 const User = ({ user }) => {
   if (!user) {
@@ -7,11 +12,12 @@ const User = ({ user }) => {
 
   return (
     <div>
-      <h2>{user.username}</h2>
-      <h3>added blogs</h3>
-      <ul>
-        {user.posts.map(post => <li key={post.id}>{post.title}</li>)}
-      </ul>
+      <Header as="h2">{user.username}</Header>
+      <Divider />
+      <Header as="h3">Added Blogs</Header>
+      <List>
+        {user.posts.map(post => <List.Item key={post.id} as="a" href={`/blogs/${post.id}`}>{post.title}</List.Item>)}
+      </List>
     </div>
   )
 }

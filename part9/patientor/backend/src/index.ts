9c229e6d676553070edc43f9e51e933f72919bf0
seenicroot <sngmanhim@hotmail.com>
2021-07-09 23:34:21 -0400

    Add route to diagnoses

--- part9/patientor/backend/src/index.ts
+++ part9/patientor/backend/src/index.ts
@@ -1,5 +1,7 @@
 import express from 'express';
 import cors from 'cors';
+import diagnosesRouter from './routes/diagnoses';
+
 const app = express();
 
 app.use(cors());
@@ -10,6 +12,8 @@ const PORT = 3001;
 app.get('/api/ping', (_req, res) => {
   res.send('pong');
 });
+
+app.use('/api/diagnoses', diagnosesRouter);
 
 app.listen(PORT, () => {
   console.log(`Server open on port ${PORT}`);

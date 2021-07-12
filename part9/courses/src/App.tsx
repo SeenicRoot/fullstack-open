ead613f83234ca6597b4bb8dab4581e286ad0031
seenicroot <sngmanhim@hotmail.com>
2021-07-12 15:41:27 -0400

    Add more complex courses with different properties.

--- part9/courses/src/App.tsx
+++ part9/courses/src/App.tsx
@@ -1,13 +1,34 @@
 import React from 'react';
 
+interface CoursePartBase {
+  name: string;
+  exerciseCount: number;
+  type: string;
+}
+
+interface CoursePartWithDescription extends CoursePartBase {
+  description: string;
+}
+
+interface CourseNormalPart extends CoursePartWithDescription {
+  type: "normal";
+}
+interface CourseProjectPart extends CoursePartBase {
+  type: "groupProject";
+  groupProjectCount: number;
+}
+
+interface CourseSubmissionPart extends CoursePartWithDescription {
+  type: "submission";
+  exerciseSubmissionLink: string;
+}
+
+type CoursePart = CourseNormalPart | CourseProjectPart | CourseSubmissionPart;
+
 const Header = ({header}: {header: string}) => (
   <h1>{header}</h1>
 )
 
-interface CoursePart {
-  name: string,
-  exerciseCount: number
-}
 const Content = ({courseParts} : {courseParts: CoursePart[]}) => (
   <div>
     {courseParts.map((p, i) => (
@@ -27,20 +48,33 @@ const Total = ({courseParts}: {courseParts: CoursePart[]}) => (
 
 const App = () => {
   const courseName = "Half Stack application development";
-  const courseParts = [
+  const courseParts: CoursePart[] = [
     {
       name: "Fundamentals",
-      exerciseCount: 10
+      exerciseCount: 10,
+      description: "This is the leisured course part",
+      type: "normal"
+    },
+    {
+      name: "Advanced",
+      exerciseCount: 7,
+      description: "This is the harded course part",
+      type: "normal"
     },
     {
       name: "Using props to pass data",
-      exerciseCount: 7
+      exerciseCount: 7,
+      groupProjectCount: 3,
+      type: "groupProject"
     },
     {
       name: "Deeper type usage",
-      exerciseCount: 14
+      exerciseCount: 14,
+      description: "Confusing description",
+      exerciseSubmissionLink: "https://fake-exercise-submit.made-up-url.dev",
+      type: "submission"
     }
-  ];
+  ]
 
   return (
     <div>
